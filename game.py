0bbc291a97506e7e4fea1020d466a4de9ffdc59b
Nick Jordan <nick@jorcademy.nl>
2023-05-28 21:40:58 +0200

    Minor changes made.

--- game.py
+++ game.py
@@ -1,19 +1,8 @@
 from jorcademy import *
 
-x = 400
-y = 300
-
-
 def setup() -> None:
-    screen(800, 600)
+    pass
 
 
 def draw() -> None:
-    global x
-    global y
-
-    x += 1
-    y += 1
-    backdrop((255, 255, 255))
-    ellipse((0, 0, 255), x, y, 100, 100)
-
+    pass
