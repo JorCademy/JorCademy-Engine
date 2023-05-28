8eaa34ad0cc7c4dca72fc8913bbfa6bc629b8fbb
Nick Jordan <nick@jorcademy.nl>
2023-05-28 17:14:11 +0200

    Minor changes.

--- game.py
+++ game.py
@@ -1,16 +1,9 @@
 from jorcademy import *
 
-RED: color = (255, 0, 0)
-GREEN: color = (0, 255, 0)
-BLUE: color = (0, 0, 255)
-
 
 def setup() -> None:
-    screen(800, 600)
-    backdrop((255, 255, 255))
+    pass
 
 
 def draw() -> None:
-    image('jorcademy.png', 400, 260, 0.4)
-    text("Think diffewent, UwU", (0, 0, 0), 400, 400)
     pass 
