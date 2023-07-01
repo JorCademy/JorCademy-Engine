b8a4a366def4cb975c6a767df68997cf909ec764
martijnv <martijnvoordouw@gmail.com>
2023-07-01 12:55:00 +0200

    Revert "basic voorbeeldje van events"
    This reverts commit 0ba516f9bf8b914169e66b4e76074f43f508f8bb.

--- game.py
+++ game.py
@@ -1,49 +1,9 @@
 from jorcademy import *
-# Circle properties
-circle_color: color = (255, 100, 100)
-circle_x: float = 400
-circle_y: float = 300
-circle_width: float = 200
-# Makes circle move based on user input
-def handle_user_input():
-    global circle_color
-    global circle_x
-    global circle_y
-    global circle_width
-    if key_up_down:
-        circle_y -= 1
-    if key_right_down:
-        circle_x += 1
-    if key_down_down:
-        circle_y += 1
-    if key_left_down:
-        circle_x -= 1
-
-
-def test():
-    print("eventtttt")
 
 
 def setup() -> None:
-    # Create a 800x600 screen
-    screen(800, 600)
-    events.key_down_event('e', test)
-
-
+    pass
 
 
 def draw() -> None:
-    global circle_color
-    global circle_x
-    global circle_y
-    global circle_width
-    # Handle movement of the circle
-    handle_user_input()
-    # Draw the backdrop
-    backdrop((255, 255, 255))
-    # Draw the circle
-    ellipse(circle_color,
-        circle_y,
-        circle_x,
-        circle_width,
-        circle_width)
+    pass
